cf5ddb42b3058b5d73c15db490403b5ff17219af
Ivan Neto <ivan.cr.neto@gmail.com>
2014-04-29 14:24:17 -0300

    fixed tabspaces and added pep8 standards

--- Engine/searchs.py
+++ Engine/searchs.py
@@ -2,23 +2,23 @@
 import re
 
 def extractGroupsText(**kwargs):
-	if  kwargs["stdin"]:
-		text = "".join(kwargs["stdin"])	#join text stdin
-	else:
-		file = open(kwargs["file"], "r")
-		text = "".join(file.readlines())
+    if  kwargs["stdin"]:
+        text = "".join(kwargs["stdin"])  # join text stdin
+    else:
+        _file = open(kwargs["file"], "r")
+        text = "".join(_file.readlines())
 
-	regex = re.compile("(?m)(?s){0}".format(kwargs["pattern"])) #compile pattern
-	for match in regex.finditer(text): #for each match in text
-		outText = kwargs["groupMatch"] #text output equal --groupMatch
-		groups = []
-		if match:
-			for group in match.groups():
-				groups.append(group)
-			for key, group in enumerate(groups):
-				regex = re.compile(r"(?<!\\)\({0}\)".format(key+1))
-				match = regex.search(str(kwargs["groupMatch"]))
-				if match:
-                  				outText = regex.sub(str(group), outText)
-             		print outText
-              		
+    # compile pattern
+    regex = re.compile("(?m)(?s){0}".format(kwargs["pattern"]))
+    for match in regex.finditer(text):  # for each match in text
+        outText = kwargs["groupMatch"]  # text output equal --groupMatch
+        groups = []
+        if match:
+            for group in match.groups():
+                groups.append(group)
+            for key, group in enumerate(groups):
+                regex = re.compile(r"(?<!\\)\({0}\)".format(key+1))
+                match = regex.search(str(kwargs["groupMatch"]))
+                if match:
+                    outText = regex.sub(str(group), outText)
+            print outText
--- pymatch.py
+++ pymatch.py
@@ -15,21 +15,21 @@ Options:
   -p, --pattern     <pattern>     pattern to find
   -g, --groupmatch  <groupmatchs> combine text with groupmatchs
   --update                        upgrade to latest version
-  
+
   Observation:    stdin works
               """
 
 def basicInfo():
-     print """
+    print """
             pyout - Text Output Tool  |  Developed by Relax Lab
               \n    Rafael Francischini (Programmer) - @rfunix\n
-              Usage: pyout.py  
+              Usage: pyout.py
                            [-p/--pattern    <pattern>    ]
                            [-g/--groupmatch <groupmatchs>]
                            [-f/--file <file>]
                            [--update   Update from github]
                            [Observation:    stdin works  ]
-                               
+
       \n              Get basic options and Help, use: -h\--help
               """
 
@@ -39,53 +39,51 @@ def createOptions():
         kwargs["stdin"] = sys.stdin.readlines()
     else:
         kwargs["stdin"] = None
-              
+
     parser = optparse.OptionParser(add_help_option=False)
-    
+
     parser.add_option("-p", "--pattern", dest="pattern", type="string",
                                         help="pattern to find",)
 
     parser.add_option("-f", "--file", dest="file", type="string",
                                         help="search GroupMatches in File",)
-    
+
     parser.add_option("--update", dest="update", action="store_true", \
                                         help="update to latest version",)
-    
+
     parser.add_option("-g", "--groupmatch", dest="groupMatch", type="string",
                                         help="combine group match with text",)
 
     parser.add_option("-h", "--help",
                       action="store_true", dest="help", help="-h")
-   
-   
+
     options, args = parser.parse_args()
-    
+
     try:
-          kwargs["groupMatch"] = options.groupMatch
-          kwargs["pattern"] = options.pattern
-          kwargs["file"] = options.file
-          
-          help = options.help
-          
-          if help:
-             printHelpMessage()
-             return
-        
-          if (kwargs["groupMatch"] == None or
+        kwargs["groupMatch"] = options.groupMatch
+        kwargs["pattern"] = options.pattern
+        kwargs["file"] = options.file
+
+        _help = options.help
+
+        if _help:
+            printHelpMessage()
+            return
+
+        if (kwargs["groupMatch"] == None or
                 kwargs["pattern"] == None or
                 (kwargs["file"]) == None and
                 kwargs["stdin"] == None):
-                    basicInfo()
-                    return
-      
-          groups = extractGroupsText(**kwargs)
-    except Exception, e:
-     basicInfo()
-     return
-    
-    
+            basicInfo()
+            return
+
+        extractGroupsText(**kwargs)
+    except Exception:
+        basicInfo()
+        return
+
 def main():
     createOptions()
-  
+
 if __name__ == "__main__":
     main()
